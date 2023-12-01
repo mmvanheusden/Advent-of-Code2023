8a9cc5743dcb1b560e58468f7ce121b30b6c8ebb
Maarten <50550545+mmvanheusden@users.noreply.github.com>
2023-12-01 18:54:51 +0100

    add day 1 shitcode

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,26 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val lines: MutableList<String> = mutableListOf() // Create list for each line.
+        val finalNumbers: MutableList<Int> = mutableListOf() // This is where all the combined numbers are stored.
+
+        // add each line to the list
+        for(line in input) {
+            lines.add(line.trim())
+        }
+
+        for (line in lines) {
+            val a = line.toSingleDigitList()
+            finalNumbers.add(("${a[0]}" + "${a[a.lastIndex]}").toInt()) // joins the first number with the last
+        }
+        return finalNumbers.sum() // sum of all numbers
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
+    //part2(input).println() todo
 }
+
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,10 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+/**
+ * Takes a string full of shit, and filters the numbers out of it.
+ */
+fun String.toSingleDigitList() = map {
+    "$it".toIntOrNull()
+}.filterNotNull()
